9bd1dec4b77cd7904c1602dc386b7ff0b50b42f0
Штефан Боблик <boblik_s@magnit.ru>
2026-05-22 00:01:37 +0300

    Initial Commit

---
+++ SimulatorDeepLinker/SimulatorDeepLinkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimulatorDeepLinkerApp.swift
+//  SimulatorDeepLinker
+//
+//  Created by Stefan Boblic on 22.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct SimulatorDeepLinkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
